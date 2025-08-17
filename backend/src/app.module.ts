5af44e7e33c15c8230c4b12ace9bd59e047d1818
Malvi <malvi.vejani@qrixe.com>
2025-08-17 14:29:43 +0530

    Fixed postgres issue

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from './modules/auth/auth.module';
-import { TasksModule } from './modules/tasks/tasks.module';
-import { ChatModule } from './modules/chat/chat.module';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "./modules/auth/auth.module";
+import { TasksModule } from "./modules/tasks/tasks.module";
+import { ChatModule } from "./modules/chat/chat.module";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
+      type: "postgres",
+      host: "localhost",
       port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'teamcollab',
+      username: "postgres",
+      password: "postgres",
+      database: "teamCollab",
       autoLoadEntities: true,
       synchronize: true,
     }),
